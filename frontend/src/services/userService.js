97ed7ddd2e197c65e0a3b27b321b4aa125844b42
Muhammad Hamza <hamzahs2004@gmail.com>
2026-09-07 03:28:54 +0500

    feat: add userService methods for editing user details

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,17 @@
+import { apiClient } from "./api";
+
+export const userService = {
+  updateAccountDetails: (data) =>
+    apiClient.patch("/api/v1/users/update-account", data),
+
+  updateAvatar: (formData) =>
+    apiClient.patch("/api/v1/users/avatar", formData, { isFormData: true }),
+
+  updateCoverImage: (formData) =>
+    apiClient.patch("/api/v1/users/cover-image", formData, {
+      isFormData: true,
+    }),
+
+  changePassword: (data) =>
+    apiClient.post("/api/v1/users/change-password", data),
+};
