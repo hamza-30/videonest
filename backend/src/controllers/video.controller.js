c417c850bd6a281fcfbb57b1232ee2c9717aaea9
Muhammad Hamza <hamzahs2004@gmail.com>
2026-09-17 00:06:00 +0500

    refactor: add owner details along with video in getVideoById controller function

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -111,15 +111,46 @@ const getVideoById = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Invalid Video ID");
   }
 
-  const video = await Video.findById(videoId);
+  const video = await Video.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(videoId) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+      },
+    },
+    {
+      $unwind: "$owner",
+    },
+    {
+      $project: {
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        duration: 1,
+        views: 1,
+        isPublished: 1,
+        createdAt: 1,
+        "owner._id": 1,
+        "owner.fullName": 1,
+        "owner.username": 1,
+        "owner.avatar": 1,
+      },
+    },
+  ]);
 
-  if (!video) {
+  if (!video?.length) {
     throw new ApiError(404, "Video not found");
   }
 
   return res
     .status(200)
-    .json(new ApiResponse(200, video, "Video fetched successfully"));
+    .json(new ApiResponse(200, video[0], "Video fetched successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
